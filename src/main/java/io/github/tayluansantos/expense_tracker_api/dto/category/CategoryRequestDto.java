0becaae64a8c4f6e32447f6acc3ca6d2d2ec4046
TayluanSantos <tayluanjesus@gmail.com>
2025-01-05 21:26:03 -0300

    feat: criando os DTOs da entidade Category

---
+++ src/main/java/io/github/tayluansantos/expense_tracker_api/dto/category/CategoryRequestDto.java
@@ -0,0 +1,4 @@
+package io.github.tayluansantos.expense_tracker_api.dto.category;
+
+public record CategoryRequestDto(String name,String description) {
+}
---
+++ src/main/java/io/github/tayluansantos/expense_tracker_api/dto/category/CategoryResponseDto.java
@@ -0,0 +1,4 @@
+package io.github.tayluansantos.expense_tracker_api.dto.category;
+
+public record CategoryResponseDto (Long id,String name,String description){
+}

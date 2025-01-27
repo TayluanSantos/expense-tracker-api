ce73c01a5e07704289500dec71a72e0555e459b0
TayluanSantos <tayluanjesus@gmail.com>
2025-01-27 18:18:39 -0300

    feat: adicionando perfis de usuários

--- src/main/java/io/github/tayluansantos/expense_tracker_api/config/SecurityConfig.java
+++ src/main/java/io/github/tayluansantos/expense_tracker_api/config/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,10 +23,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         return http.
                 csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/users/login").permitAll();
                     auth.requestMatchers(HttpMethod.POST,"/users/**").permitAll();
+                    auth.requestMatchers("/expenses/**").hasAnyRole("USER","ADMIN");
+                    auth.requestMatchers("/categories/**").hasRole("ADMIN");
                     auth.anyRequest().authenticated();
                 })
                 .build();

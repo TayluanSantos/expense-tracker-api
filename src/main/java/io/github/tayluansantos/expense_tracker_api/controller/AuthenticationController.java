e981fe64d38e518e0d1be48bc5c964a7939ec082
TayluanSantos <tayluanjesus@gmail.com>
2025-01-28 13:25:35 -0300

    feat: adicionando a AuthController

--- src/main/java/io/github/tayluansantos/expense_tracker_api/controller/AuthenticationController.java
+++ src/main/java/io/github/tayluansantos/expense_tracker_api/controller/AuthenticationController.java
@@ -1,4 +1,36 @@
 package io.github.tayluansantos.expense_tracker_api.controller;
 
+import io.github.tayluansantos.expense_tracker_api.dto.user.UserLoginDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody UserLoginDto userLogin){
+
+        try {
+            UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(userLogin.email(), userLogin.password());
+            Authentication authenticate = this.authenticationManager.authenticate(usernamePassword);
+        } catch (BadCredentialsException ex){
+            throw new BadCredentialsException("Username or password invalid");
+        }
+
+        return ResponseEntity.ok().build();
+    }
 }

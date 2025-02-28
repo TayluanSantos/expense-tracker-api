f3b1da8fc5a99493c5832d2dd0558128d1d8b5ba
TayluanSantos <tayluanjesus@gmail.com>
2025-02-28 11:52:59 -0300

    docs: addocumentando a CategoryController com o Swagger

--- src/main/java/io/github/tayluansantos/expense_tracker_api/controller/CategoryController.java
+++ src/main/java/io/github/tayluansantos/expense_tracker_api/controller/CategoryController.java
@@ -3,6 +3,10 @@ package io.github.tayluansantos.expense_tracker_api.controller;
 import io.github.tayluansantos.expense_tracker_api.dto.category.CategoryRequestDto;
 import io.github.tayluansantos.expense_tracker_api.dto.category.CategoryResponseDto;
 import io.github.tayluansantos.expense_tracker_api.service.ICategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@Tag(name = "Category",description = "Endpoints para criação, atualização, listagem e remoção de categorias no sistema.")
 public class CategoryController {
 
     private final ICategoryService categoryService;
@@ -20,28 +25,62 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @Operation(summary = "Cadastrar uma nova categoria", description = "Recebe os dados de uma nova categoria e as cadastra no sistema, retornando os dados da categoria criada.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Categoria criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida. Verifique os campos obrigatórios."),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado"),
+            @ApiResponse(responseCode = "500", description = "Erro de servidor interno")
+    })
     @PostMapping
     public ResponseEntity<CategoryResponseDto> save(@RequestBody @Valid CategoryRequestDto categoryRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.save(categoryRequestDto));
     }
 
+    @Operation(summary = "Obter detalhes de uma categorias pelo ID", description = "Retorna os detalhes de uma categoria específica com base no ID informado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado"),
+            @ApiResponse(responseCode = "500", description = "Erro de servidor interno")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CategoryResponseDto> findById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.findById(id));
     }
 
-
+    @Operation(summary = "Obter a lista de categorias cadastradas", description = "Retorna todas as categorias cadastradas no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de categorias retornada com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado"),
+            @ApiResponse(responseCode = "500", description = "Erro de servidor interno")
+    })
     @GetMapping
     public ResponseEntity<List<CategoryResponseDto>> findAll () {
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.findAll());
     }
 
+    @Operation(summary = "Atualizar as informações de uma categoria", description = "Atualiza os dados de uma categoria existente com base no ID informado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida. Verifique os campos obrigatórios."),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado"),
+            @ApiResponse(responseCode = "500", description = "Erro de servidor interno")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponseDto> update(@RequestBody CategoryRequestDto categoryRequest,
                                                      @PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.update(categoryRequest,id));
     }
 
+    @Operation(summary = "Remover uma categoria", description = "Exclui uma categoria do sistema com base no ID informado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Categoria excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autenticado"),
+            @ApiResponse(responseCode = "500", description = "Erro de servidor interno")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         categoryService.delete(id);

983e2b47fc58ad753e86bbabc7e62543a5267375
TayluanSantos <tayluanjesus@gmail.com>
2025-03-05 22:56:48 -0300

    feat: adicionando logging a classe CategoryServiceImpl

--- src/main/java/io/github/tayluansantos/expense_tracker_api/service/CategoryServiceImpl.java
+++ src/main/java/io/github/tayluansantos/expense_tracker_api/service/CategoryServiceImpl.java
@@ -6,6 +6,8 @@ import io.github.tayluansantos.expense_tracker_api.exception.ResourceNotFoundExc
 import io.github.tayluansantos.expense_tracker_api.mapper.ICategoryMapper;
 import io.github.tayluansantos.expense_tracker_api.model.Category;
 import io.github.tayluansantos.expense_tracker_api.repository.CategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService{
+
+    private static final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     private final CategoryRepository categoryRepository;
     private final ICategoryMapper categoryMapper;
@@ -25,41 +29,73 @@ public class CategoryServiceImpl implements ICategoryService{
 
     @Override
     public List<CategoryResponseDto> findAll() {
+
+        logger.info("Retrieving all categories");
         return categoryMapper.convertListEntityToListDto(categoryRepository.findAll());
     }
 
     @Override
     public CategoryResponseDto findById(Long id) {
+
+        logger.info("Attempting to find a category by id");
+
         Category category = categoryRepository
                 .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Cannot found category with id:" + id));
-        return categoryMapper.convertToDto(category);
+                .orElseThrow(()-> {
+                    logger.warn("Failed to find a category with id:{}",id);
+                    return new ResourceNotFoundException("Cannot found category with id:" + id);
+                });
+        CategoryResponseDto categoryResponseDto = categoryMapper.convertToDto(category);
+        logger.info("Category with id {} was found succsessfully",id);
+        return categoryResponseDto ;
     }
 
     @Override
     public CategoryResponseDto save(CategoryRequestDto categoryRequestDto) {
+
+        logger.info("Attempting to create a category");
+
         Category category = categoryRepository
                 .save(categoryMapper.convertToEntity(categoryRequestDto));
 
-        return categoryMapper.convertToDto(category);
+        CategoryResponseDto categoryResponseDto = categoryMapper.convertToDto(category);
+        logger.info("Category created successfully");
+
+        return categoryResponseDto;
     }
 
     @Override
     public CategoryResponseDto update(CategoryRequestDto categoryRequestDto, Long id) {
+
+        logger.info("Attempting to update category with id:{}",id);
+
         Category category = categoryRepository
                 .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Cannot found category with id:" + id));
+                .orElseThrow(()-> {
+                    logger.warn("Failed to find a category with id:{}",id);
+                    return new ResourceNotFoundException("Cannot found category with id:" + id);
+                });
         category.setName(categoryRequestDto.name());
         category.setDescription(categoryRequestDto.description());
 
-        return categoryMapper.convertToDto(categoryRepository.save(category));
+        CategoryResponseDto categoryResponseDto = categoryMapper.convertToDto(categoryRepository.save(category));
+        logger.info("Category with id {} was updated succsessfully",id);
+
+        return categoryResponseDto;
     }
 
     @Override
     public void delete(Long id) {
+
+        logger.info("Attempting to delete category with id:{}",id);
+
         Category category = categoryRepository
                 .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Cannot found category with id:" + id));
+                .orElseThrow(()-> {
+                    logger.warn("Failed to find a category with id:{}",id);
+                    return new ResourceNotFoundException("Cannot found category with id:" + id);
+                });
         categoryRepository.delete(category);
+        logger.info("Category with id {} was deleted succsessfully",id);
     }
 }
